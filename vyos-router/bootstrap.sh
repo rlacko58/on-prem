25deaa14fea220648007ea51d45c6335e4d282f6
Laszlo Rafael <rlacko99@gmail.com>
2023-01-29 19:33:56 +0100

    Add vyos router setup and pipeline

---
+++ vyos-router/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# This script creates a Python env, enters it and installs
+# ansible and requirements from the requirements.galaxy.yaml
+
+[[ ! -d .venv ]] && python3 -m venv .venv
+source .venv/bin/activate
+
+pip3 install ansible ansible-lint ansible-pylibssh
+
+ansible-galaxy install "$1" -r requirements.galaxy.yaml
+
+# to stay in our comfy virtualenv
+exec "${SHELL:bash}"
